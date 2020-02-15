a88cc6ccb5d39bf7d6369dabcd81d8916d4c8a15
davidcaoyh <yuhaocao98@gmail.com>
2020-02-14 19:55:36 -0500

    pushed

---
+++ functions.h
@@ -0,0 +1,11 @@
+#pragma once
+class functions
+{
+
+};
+void greetAndInstruct();
+void displayBoard(char board[]);
+bool checkIfLegal(int cellNbre, char boardp[]);
+bool checkWinner(char board[]);
+bool checkTie(char board[]);
+void computerMove(char board[]);
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "functions.h"
+#include <iostream>
+using namespace std;
+int main() {
+	greetAndInstruct();
+	char board[27];
+	for (int i = 0; i < 27; i++) {
+		board[i] = ' ';
+	}
+	char input;
+	cin >> input;
+	while (input != 'y' && input != 'n') {
+		cout << "Invalid, enter again" << endl;
+		cin >> input;
+	}
+	if (input == 'n') {
+		exit(0);
+	}
+	displayBoard(board);
+	while (checkWinner(board) == false && checkTie(board) == false) {
+		int in;
+		cin >> in;
+		while (checkIfLegal(in, board) == false) {
+			cin >> in;
+		}
+		cout << "board" << endl;
+		displayBoard(board);
+
+		if (checkTie(board)) {
+			cout << "tie" << endl;
+			exit(0);
+		}
+		if (checkWinner(board)) {
+			cout << "Player wins" << endl;
+			exit(0);
+		}
+		computerMove(board);
+		cout << "board:" << endl;
+		displayBoard(board);
+		if (checkTie(board)) {
+			cout << "tie" << endl;
+			exit(0);
+		}
+		if (checkWinner(board)) {
+			cout << "ai wins" << endl;
+			exit(0);
+		}
+
+
+	}
+}
